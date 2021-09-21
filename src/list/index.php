27f5b7e27f84c07c588104e209f8c0a98b01f6a3
David M <davidmusa1@hotmail.com>
2021-09-21 15:50:17 -0400

    fixing database demands

--- src/list/index.php
+++ src/list/index.php
@@ -1,26 +1,35 @@
+<?php
+
+require_once "config.php";
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>TO DO</title>
 </head>
 <body>
     <div id="demo">
         TO DO: <input type="text" name="name"><br>
-        <input type="submit" onclick="loadDoc()">
+        <input type="submit" id="submit" name="submit" onclick="loadDoc()">
     </div>
 
     <script>
         function loadDoc() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("demo").innerHTML =
-                this.responseText;
-            }
-        };
+
+            var loader = '<div class="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>';    
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("demo").innerHTML = this.responseText;
+                }
+            };
         xhttp.open("GET", "list.php", true);
         xhttp.send();
         }
---
+++ src/list/list.php
@@ -0,0 +1,35 @@
+<?php
+
+    /*
+    * Date: 2021-08-30  
+    * Author: David Musa
+    * Description: This file contains The Database Connection.
+    */
+
+    //Initialize session
+    session_start();
+
+    //Requires files
+    require_once "config.php";
+
+    if (isset($_POST['submit'])){
+
+    // Escape user inputs for security
+    $todo= mysqli_real_escape_string(
+        $link, $_REQUEST['todo']);
+    date_default_timezone_set('America/Toronto');
+        $date=date('y-m-d h:ia');
+
+    // Attempt insert query execution
+    $sql = "INSERT INTO list (todo, date_time) VALUES ('$todo', '$date')";
+    if(mysqli_query($link, $sql)){
+    ;
+    } else {
+    echo "ERROR: Message not sent!!!";
+    }
+    // Close connection
+    mysqli_close($link);
+
+    }
+    
+?>
